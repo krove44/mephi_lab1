9da7e7717e2fb88ff1281860a73cdcbba7c798c8
krove44 <krove8@gmail.com>
2026-03-17 23:52:53 +0300

    add cmake

---
+++ build/CMakeFiles/text_search.dir/cmake_clean.cmake
@@ -0,0 +1,15 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/text_search.dir/data_structure.c.obj"
+  "CMakeFiles/text_search.dir/data_structure.c.obj.d"
+  "CMakeFiles/text_search.dir/main.c.obj"
+  "CMakeFiles/text_search.dir/main.c.obj.d"
+  "libtext_search.dll.a"
+  "text_search.exe"
+  "text_search.exe.manifest"
+  "text_search.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang C)
+  include(CMakeFiles/text_search.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
---
+++ build/CMakeFiles/text_search.dir/typeinfo.c.obj.d
@@ -0,0 +1,3 @@
+CMakeFiles/text_search.dir/typeinfo.c.obj: \
+ C:\Users\krove44\Desktop\hard_laba\typeinfo.c \
+ C:\Users\krove44\Desktop\hard_laba\typeinfo.h
